8facd977581b9253296478053b3f1b470d5ed86b
Jicei <31828588+Jicei@users.noreply.github.com>
2020-12-03 14:23:08 +0200

    Add Migration

--- EducationOnlinePlatform/Migrations/20201203122125_DescriptionAdd.Designer.cs
+++ EducationOnlinePlatform/Migrations/20201203122125_DescriptionAdd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using EducationOnlinePlatform;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EducationOnlinePlatform.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20201203122125_DescriptionAdd")]
+    partial class DescriptionAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
