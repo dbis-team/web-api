4bd519f4b1493d9b7c0234bd2db40bc35c203d4e
Jicei <31828588+Jicei@users.noreply.github.com>
2020-12-19 15:16:06 +0200

    Logger Fix

--- EducationOnlinePlatform/Startup.cs
+++ EducationOnlinePlatform/Startup.cs
@@ -78,7 +78,7 @@ namespace EducationOnlinePlatform
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationContext context)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationContext context, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
